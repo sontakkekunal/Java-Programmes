3f3de48684381c8b3094dc818939361c74c62cd4
sontakkekunal <sontakkekunal2004@gmail.com>
2023-07-30 14:33:05 +0530

    Renaming files2

--- Java_Class Programme/Control Statement/Basic If else/Assignment/Q1_programme1.java
+++ Java_Class Programme/Control Statement/Basic If else/Assignment/Q1_programme1.java
@@ -2,15 +2,13 @@
 //1: Write a java program to check if a number is even or odd.
 class EvenOdd{
 	public static void main(String[] pp){
-		int a=5;
-		if(a%2==0){
+		int a=0;
+		//0%2-->0
+		if(a%2==0){     
 			System.out.println(a+" is an even number.");
 		}
-		else if(a%2==1){
+		else {
 			System.out.println(a+" is an odd number.");
-		}
-		else{
-			System.out.println(a+" is an even number.");
 		}
 	}
 }
